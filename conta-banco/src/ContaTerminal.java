64ec499b3a4436bc49c2c4eb78927da3a6bfcde3
Adriazus Figueiredo <adriazusdev@gmail.com>
2024-08-17 17:58:23 -0400

    Atualização do código

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,36 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        //Criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        
+        //String nomeCliente = args [0];
+        //String agencia = args [1];
+        //int nrAgencia = Interger.valueOf(args[2]);
+        //double saldo = Double.valueOf(args[3]);
+
+        System.out.println("Digite Seu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+        
+        System.out.println("Digite sua agencia");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o numero da sua conta (use somente numeros)");
+        int nrConta = scanner.nextInt();
+
+        System.out.println("Digite seu saldo");
+        double saldo = scanner.nextDouble();
+
+        //imprimindo os dados obtidos
+        System.out.println("Seja bem vindo " + nome + " " + sobrenome);
+        System.out.println("O numero da sua agencia é " + agencia);
+        System.out.println("O numero da sua conta é " + nrConta);
+        System.out.println("O seu saldo atual disponivel para saque é " + saldo);
     }
 }
